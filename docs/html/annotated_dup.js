68ea13048863c7d6aa3288b331806c9a0f6e6f2b
amoradinejad <azadeh.moradinejad@gmail.com>
2021-11-05 16:58:25 +0100

    updated doxygen Doxyfile

--- docs/html/annotated_dup.js
+++ docs/html/annotated_dup.js
@@ -1,9 +1,18 @@
 var annotated_dup =
 [
+    [ "cacheval_s", "d1/da0/structcacheval__s.html", null ],
     [ "Class_Cosmology_Struct", "d2/dd8/struct_class___cosmology___struct.html", null ],
     [ "Cosmology", "d2/dbd/struct_cosmology.html", null ],
+    [ "esterr", "d2/d85/structesterr.html", null ],
+    [ "fv_data_s", "d3/da1/structfv__data__s.html", null ],
     [ "globals", "d8/d11/structglobals.html", null ],
+    [ "heap", "db/dc3/structheap.html", null ],
+    [ "hypercube", "d2/d1d/structhypercube.html", null ],
     [ "integrand_parameters", "d5/dd6/structintegrand__parameters.html", null ],
     [ "integrand_parameters2", "d0/d4a/structintegrand__parameters2.html", null ],
-    [ "Line", "d2/d9d/struct_line.html", null ]
+    [ "Line", "d2/d9d/struct_line.html", null ],
+    [ "region", "d9/d61/structregion.html", null ],
+    [ "rule75genzmalik", "d1/d65/structrule75genzmalik.html", null ],
+    [ "rule_s", "d0/df5/structrule__s.html", null ],
+    [ "valcache_s", "df/d72/structvalcache__s.html", null ]
 ];
--- docs/html/search/all_e.js
+++ docs/html/search/all_e.js
@@ -2,5 +2,8 @@ var searchData=
 [
   ['r_5fscale_0',['R_scale',['../d6/d32/cosmology_8c.html#a7558ae7e109dad93059a1f88b2b4861f',1,'cosmology.c']]],
   ['r_5fvir_1',['R_vir',['../d6/d32/cosmology_8c.html#a4dc96bb7695696838c30e0929302c99c',1,'cosmology.c']]],
-  ['rhoc_2',['rhoc',['../d6/d32/cosmology_8c.html#a01ee8bb070f25ca114ae5d0b52d200a7',1,'cosmology.c']]]
+  ['region_2',['region',['../d9/d61/structregion.html',1,'']]],
+  ['rhoc_3',['rhoc',['../d6/d32/cosmology_8c.html#a01ee8bb070f25ca114ae5d0b52d200a7',1,'cosmology.c']]],
+  ['rule75genzmalik_4',['rule75genzmalik',['../d1/d65/structrule75genzmalik.html',1,'']]],
+  ['rule_5fs_5',['rule_s',['../d0/df5/structrule__s.html',1,'']]]
 ];
